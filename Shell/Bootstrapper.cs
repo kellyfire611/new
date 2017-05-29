cb64545f07101c4dc9f139aeb12d439cb792c65a
kellyfire611 <kellyfire611@gmail.com>
2017-05-29 16:47:36 +0700

    Phân tách các module.

---
+++ Shell/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using System.Windows;
+using Prism.Modularity;
+using System.IO;
+using ShellWindow.Views;
+
+namespace Shell
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            string modulePath = @".\Modules";
+            if (!Directory.Exists(modulePath))
+            {
+                Directory.CreateDirectory(modulePath);
+            }
+            return new DirectoryModuleCatalog() { ModulePath = modulePath };
+        }
+    }
+}
